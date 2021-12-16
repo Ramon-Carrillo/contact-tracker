3b6e2b3b82e490a743b2406be2c67442715f5421
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-12-16 16:08:13 -0600

    added auth context and initial state.

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/contactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/about' element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/about' element={<About />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
